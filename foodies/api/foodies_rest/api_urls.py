541bc6be713cea8f3c33a1f02a7e59cb8c19d0ef
David Quinlan <dquinlan07@gmail.com>
2022-06-03 18:53:19 -0500

    foodies acls keys views work

--- foodies/api/foodies_rest/api_urls.py
+++ foodies/api/foodies_rest/api_urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import(
     api_list_foodies,
+    api_get_yelp,
 )
 
 urlpatterns = [
     path("foodies/", api_list_foodies, name="api_list_foodies"),
+    path("yelp/", api_get_yelp, name="api_get_yelp"),
 ]
