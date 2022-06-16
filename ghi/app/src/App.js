79b50812530a2568c8a2c56fdf9cbdaae6ed0fdc
Brandon <brandon.c.dix@gmail.com>
2022-06-16 15:17:43 -0700

    react testing

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,52 +1,39 @@
-import { useState } from 'react';
-import './App.css';
+// import { useState } from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useToken } from './authApi';
+import Nav from "./Nav"
+import Login from "./users/Login"
+import SignUp from "./users/SignUp"
+import Logout from "./users/Logout"
+import HomePage from "./HomePage"
+import Footer from "./Footer"
+import './App.css';
 
 function App() {
   const [token, login, logout, signup] = useToken();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [isOwner, setIsOwner] = useState(false);
-  const [isFoodie, setIsFoodie] = useState(false);
-  const [error, setError] = useState(null);
 
   return (
-    // <BrowserRouter>
-    //   <Nav token={token} />
-    //   <Routes>
-    //     <Route path="" element={<Login token={token} login={login} />} />
-    //   </Routes>
-    // </BrowserRouter>
-    <div>
-      { error ? <div dangerouslySetInnerHTML={{__html: error}} /> : null }
-      { token
-      ? <div>
-          <h1>Logout</h1>
-          <div>{token}</div>
-          <button onClick={logout}>Logout</button>
-        </div>
-      : <div>
-          <h1>Login</h1>
-          <div>
-          <input type="checkbox" checked={isFoodie} onChange={e => setIsFoodie(e.target.checked)} />
-            Is foodie
-          </div>
-          <div>
-            <input type="checkbox" checked={isOwner} onChange={e => setIsOwner(e.target.checked)} />
-            Is owner
-          </div>
-          <input type="text" onChange={e => setUsername(e.target.value)} value={username} placeholder="username" />
-          <input type="email" onChange={e => setEmail(e.target.value)} value={email} placeholder="email" />
-          <input type="phone" onChange={e => setPhone(e.target.value)} value={phone} placeholder="phone" />
-          <input type="password" onChange={e => setPassword(e.target.value)} value={password} placeholder="password" />
-          <button onClick={async () => setError(await login(username, password))}>Login</button>
-          <button onClick={async () => setError(await signup(username, password, email, phone, isOwner, isFoodie))}>Sign up</button>
-        </div>
-      }
-    </div>
+    <BrowserRouter>
+      <Nav token={token} />
+      <div className='container-fluid p-0'>
+        <Routes>
+          <Route Path='users'>
+            <Route path='login' element={<Login token={token} login={login} />} />
+            <Route path='logout' element={<Logout logout={logout} />} />
+            <Route
+              path='signup'
+              element={<SignUp token={token} signup={signup} />}
+            />
+          </Route>
+          {/* <Route Path="/" element = {HomePage} /> */}
+        </Routes>
+      </div>
+      {/* <div className='footer-container'>
+        <Footer />
+      </div> */}
+    </BrowserRouter>
   );
 }
-
 export default App;
+
