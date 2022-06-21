0f086317f15aa30b5f603154f1a0f9564a34ee53
Cameron Guadagnino <cjguad@gmail.com>
2022-06-21 16:27:47 -0700

    removed useless volumes

--- ghi/app/src/users/SignUp.js
+++ ghi/app/src/users/SignUp.js
@@ -28,63 +28,65 @@ function SignUp(props) {
     return <Navigate to="/" />;
   }
   return (
-    <CardWrapper>
-      <CardHeader>
-        <CardHeading>Welcome to Eatinerary!</CardHeading>
-      </CardHeader>
+    <div className="container my-5">
+      <CardWrapper>
+        <CardHeader>
+          <CardHeading>Welcome to Eatinerary!</CardHeading>
+        </CardHeader>
 
-      <CardBody>
-        <CardFieldset>
-          <CardBody> Foodie 
-          <CardInput onChange={e => setIsFoodie(e.target.checked)} checked={isFoodie} placeholder="Foodie?" type="checkbox" />
-          </CardBody>        
-        </CardFieldset>
-      </CardBody>
-      
-      <CardBody>
-        <CardFieldset>
-          <CardBody> Owner 
-          <CardInput onChange={e => setIsOwner(e.target.checked)} checked={isOwner} placeholder="Owner?" type="checkbox" />
-          </CardBody>        
-        </CardFieldset>
-      </CardBody>
-
-      <CardBody>
-        <CardFieldset>
-          <CardInput onChange={e => setUsername(e.target.value)} value={username} placeholder="Username" type="text" required />
-        </CardFieldset>
+        <CardBody>
+          <CardFieldset>
+            <CardBody> Foodie 
+            <CardInput onChange={e => setIsFoodie(e.target.checked)} checked={isFoodie} placeholder="Foodie?" type="checkbox" />
+            </CardBody>        
+          </CardFieldset>
+        </CardBody>
         
-        <CardFieldset>
-          <CardInput onChange={e => setPassword(e.target.value)} value={password} placeholder="Password" type="password" required />
-          <CardIcon className="fa fa-eye" eye small />
-        </CardFieldset>
+        <CardBody>
+          <CardFieldset>
+            <CardBody> Owner 
+            <CardInput onChange={e => setIsOwner(e.target.checked)} checked={isOwner} placeholder="Owner?" type="checkbox" />
+            </CardBody>        
+          </CardFieldset>
+        </CardBody>
 
-        <CardFieldset>
-          <CardInput onChange={e => setFirstName(e.target.value)} value={first_name} placeholder="First Name" type="text" required />
-        </CardFieldset>
+        <CardBody>
+          <CardFieldset>
+            <CardInput onChange={e => setUsername(e.target.value)} value={username} placeholder="Username" type="text" required />
+          </CardFieldset>
+          
+          <CardFieldset>
+            <CardInput onChange={e => setPassword(e.target.value)} value={password} placeholder="Password" type="password" required />
+            <CardIcon className="fa fa-eye" eye small />
+          </CardFieldset>
 
-        <CardFieldset>
-          <CardInput onChange={e => setLastName(e.target.value)} value={last_name} placeholder="Last Name" type="text" required />
-        </CardFieldset>
+          <CardFieldset>
+            <CardInput onChange={e => setFirstName(e.target.value)} value={first_name} placeholder="First Name" type="text" required />
+          </CardFieldset>
 
-        <CardFieldset>
-          <CardInput onChange={e => setEmail(e.target.value)} value={email} placeholder="Email" type="email" required />
-        </CardFieldset>
+          <CardFieldset>
+            <CardInput onChange={e => setLastName(e.target.value)} value={last_name} placeholder="Last Name" type="text" required />
+          </CardFieldset>
 
-        <CardFieldset>
-          <CardInput onChange={e => setPhone(e.target.value)} value={phone} placeholder="Phone" type="text" required />
-        </CardFieldset>
+          <CardFieldset>
+            <CardInput onChange={e => setEmail(e.target.value)} value={email} placeholder="Email" type="email" required />
+          </CardFieldset>
 
-        <CardFieldset>
-          <CardButton onClick={() => signup(username, password, first_name, last_name, email, phone )} type="button">Become A Member</CardButton>
-        </CardFieldset>
-        <CardFieldset>
-          <NavLink to="/login">
-            <CardLink>Sign in Here</CardLink>
-          </NavLink>
-        </CardFieldset>
-      </CardBody>
-    </CardWrapper>
+          <CardFieldset>
+            <CardInput onChange={e => setPhone(e.target.value)} value={phone} placeholder="Phone" type="text" required />
+          </CardFieldset>
+
+          <CardFieldset>
+            <CardButton onClick={() => signup(username, password, first_name, last_name, email, phone )} type="button">Become A Member</CardButton>
+          </CardFieldset>
+          <CardFieldset>
+            <NavLink to="/login">
+              <CardLink>Sign in Here</CardLink>
+            </NavLink>
+          </CardFieldset>
+        </CardBody>
+      </CardWrapper>
+    </div>
   );
 }
 
