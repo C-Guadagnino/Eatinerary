7dcb502bab7467939a4c7d69dbc0499b7d07db70
David Quinlan <dquinlan07@gmail.com>
2022-06-22 17:14:04 -0500

    latest from main

--- ghi/app/src/foodies/GoogleMaps2.js
+++ ghi/app/src/foodies/GoogleMaps2.js
@@ -11,7 +11,7 @@ class GoogleMaps extends React.Component {
             <div>
 
 
-                <Iframe url={`https://www.google.com/maps/embed/v1/place?key=&q=${this.props.name}+${this.props.city}+${this.props.state}&center=${this.props.latitude},${this.props.longitude}`}
+                <Iframe url={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAu_o8QZpXb6L161cw5FJS6v3g4k2VlZlE&q=${this.props.name}+${this.props.city}+${this.props.state}&center=${this.props.latitude},${this.props.longitude}`}
                         width="700px"
                         height="350px"
                         id="mySkewered"
