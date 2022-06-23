529a1831c3b4364c2dc75fb861d4ffc26a48e05a
Brandon <brandon.c.dix@gmail.com>
2022-06-22 19:31:33 -0700

    speacial date form in the works

--- ghi/app/src/foodies/SpecialDatesForm.js
+++ ghi/app/src/foodies/SpecialDatesForm.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
-class AutoForm extends React.Component {
+class SpecialDateForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             specialDate: '',
             occasion: '',
-            hasPassed: '',
             repeats: '',
-            frequency: [],
+            frequency: '',
             foodieVO: []
         };
 
         this.handleSpecialDateChange = this.handleSpecialDateChange.bind(this);
         this.handleOccasionChange = this.handleOccasionChange.bind(this);
-        this.handleHasPassedChange = this.handleHasPassedChange.bind(this);
         this.handleRepeatsChange = this.handleRepeatsChange.bind(this);
         this.handleFrequencyChange = this.handleFrequencyChange.bind(this);
         this.handleFoodieVoChange = this.handleFoodieVoChange.bind(this);
@@ -31,10 +29,6 @@ class AutoForm extends React.Component {
         this.setState({occasion: value})
     }
 
-    handleHasPassedChange(event) {
-        const value = event.target.value;
-        this.setState({hasPassed: value})
-    }
 
     handleRepeatsChange(event) {
         const value = event.target.value;
@@ -54,9 +48,13 @@ class AutoForm extends React.Component {
     async handleSubmit(event) {
         event.preventDefault();
         const data = {...this.state};
-        delete data.models
+        const special_date = data.special_date
+        const foodie_vo = data.foodie_vo
+        delete data.specialDate
+        delete data.foodie_vo
         
-        const appointmentUrl = `http://localhost:8100/api/foodies/${this.state.foodieVO}/specialdates/`;
+        
+        const appointmentUrl = `http://localhost:8100/api/foodies/${this.state.foodie_vo.username}/specialdates/`;
         const fetchConfig = {
             method: "post",
             body: JSON.stringify(data),
@@ -83,9 +81,8 @@ class AutoForm extends React.Component {
             const cleared = {
                 specialDate: '',
                 occasion: '',
-                hasPassed: '',
                 repeats: '',
-                frequency: [],
+                frequency: '',
                 foodieVO: []
                 };
             this.setState(cleared);
@@ -111,30 +108,27 @@ class AutoForm extends React.Component {
             <div className="row">
                     <div className="offset-3 col-6">
                         <div className="shadow p-4 mt-4">
-                            <h1>Create a new Automobile</h1>
+                            <h1>Add A Special Date</h1>
                             <form onSubmit={this.handleSubmit} id="create-shoe-form">
                                 <div className="form-floating mb-3">
-                                    <input onChange={this.handleColorChange} value={this.state.color} placeholder="color" required type="text" name="color" id="color" className="form-control" />
-                                    <label htmlFor="color">Color</label>                     
+                                    <input onChange={this.handleSpecialDateChange} value={this.state.specialDate} placeholder="specialDate" required type="text" name="specialDate" id="specialDate" className="form-control" />
+                                    <label htmlFor="specialDate">Special Date</label>                     
                                 </div>
                                 <div className="form-floating mb-3">
-                                    <input onChange={this.handleYearChange} value={this.state.year} placeholder="year" required type="text" name="year" id="year" className="form-control" />
-                                    <label htmlFor="year">Year</label>                     
+                                    <input onChange={this.handleOccasionChange} value={this.state.occasion} placeholder="occasion" required type="text" name="occasion" id="occasion" className="form-control" />
+                                    <label htmlFor="occasion">Occasion</label>                     
                                 </div>
                                 <div className="form-floating mb-3">
-                                    <input onChange={this.handleVinChange} value={this.state.vin} placeholder="vin" required type="text" name="vin" id="vin" className="form-control" />
-                                    <label htmlFor="reason">VIN (17 Characters)</label>                     
+                                    <input onChange={this.handleRepeatsChange} value={this.state.repeats} placeholder="repeats" required type="text" name="repeats" id="repeats" className="form-control" />
+                                    <label htmlFor="repeats">Does this occasion repeat?</label>                     
                                 </div>
                                 <div className="mb-3">
-                                    <select onChange={this.handleModelChange} value={this.state.model_id} required id="model" name="model" className="form-select">
-                                        <option value="">Choose the Vehicle</option>
-                                        {this.state.models.map(model_id => {
-                                            return (
-                                                <option key={model_id.id} value={model_id.id}>{model_id.name}</option>
-                                            );
-                                        })}
+                                    <select onChange={this.handleFrequencyChange} value={this.state.frequency} id="frequency" name="frequency" className="form-select">
+                                        <option value="">How often does this occasion repeat?</option>
+                                        <option key="0" value="Monthly">Monthly</option>
+                                        <option key="1" value="Yearly">Yearly</option>
                                     </select>
-                                  </div>                    
+                                </div>                             
                                 <button className="btn btn-primary">Create</button>
                             </form>
                         </div>
@@ -144,4 +138,4 @@ class AutoForm extends React.Component {
     }
 }
 
-export default AutoForm;
+export default SpecialDateForm;
